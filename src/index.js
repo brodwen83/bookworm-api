5f60ff4492fb315be3ae0190ed14e71e93f78b97
wendell <brodwen83@gmail.com>
2018-11-15 09:47:32 +0800

    mongodb and bcrypt installed. Now validates password and return users email

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+import auth from "./routes/auth";
 
 const app = express();
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { global: "Invalid credentials" } });
-});
+app.use(bodyParser.json());
+
+mongoose.connect(
+  "mongodb://localhost/bookworm",
+  { useMongoClient: true }
+);
+
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(9100, () => console.log("Running on localhost:9100"));
+app.listen(8080, () => console.log("Running on localhost:8080"));
